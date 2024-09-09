cab4fd94d05ec7a6d0a7ebb4c8dc204c7186df99
noobnuby <noobnuby77@gmail.com>
2024-09-09 22:40:00 +0900

    update support for multiple file extensions

--- file-server.js
+++ file-server.js
@@ -6,6 +6,7 @@ let file = process.argv[2];
 let port = parseInt(process.argv[3]);
 
 http.createServer((req, res) => {
+    res.setHeader('Content-Disposition', `attachment; filename=${encodeURIComponent(file)}`);
     fs.createReadStream(file).pipe(res);
     let ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
     console.log(`${ip} requested.`);
--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const http = require("http");
 module.exports = (file, port, callback = () => {}) => {
     if (typeof file === "string") {
         const fs = require("fs");
-        http.createServer((req, res) => fs.createReadStream(file).pipe(res))
+        http.createServer((req, res) => {
+            res.setHeader('Content-Disposition', `attachment; filename=${encodeURIComponent(file)}`)
+            fs.createReadStream(file).pipe(res)
+        })
         .listen(port, callback);
     } else {
         http.createServer((req, res) => res.end(file))
